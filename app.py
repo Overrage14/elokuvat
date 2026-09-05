562f67df8adaed326b697c12a11c5b35777425de
Overrage14 <valtteri.litja@gmail.com>
2026-09-05 18:58:00 +0300

    Add user registration and login

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import secrets
+import sqlite3
+
 from flask import Flask
-from flask import render_template
+from flask import abort, flash, redirect, render_template, request, session
 
 import config
+import users
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
@@ -9,3 +13,53 @@ app.secret_key = config.secret_key
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if not username or len(username) > 30:
+        abort(403)
+    if not password1 or len(password1) > 100:
+        abort(403)
+    if password1 != password2:
+        flash("VIRHE: salasanat eivät ole samat")
+        return redirect("/register")
+
+    try:
+        users.create_user(username, password1)
+    except sqlite3.IntegrityError:
+        flash("VIRHE: tunnus on jo varattu")
+        return redirect("/register")
+
+    return redirect("/")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user_id = users.check_login(username, password)
+        if user_id:
+            session["user_id"] = user_id
+            session["username"] = username
+            session["csrf_token"] = secrets.token_hex(16)
+            return redirect("/")
+        flash("VIRHE: väärä tunnus tai salasana")
+        return redirect("/login")
+
+@app.route("/logout")
+def logout():
+    if "user_id" in session:
+        del session["user_id"]
+        del session["username"]
+    return redirect("/")
